a8143e98301016fdf4b6970f20985f6720973e13
Adilet Tursunbekov <adileturs@gmai.com>
2025-07-04 10:34:14 +0600

    feat(auth): configure database connection and env variables

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,33 @@
+// Путь: apps/auth/src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Модуль для работы с переменными окружения.
+    // Он загрузит переменные из нашего .env файла.
+    ConfigModule.forRoot({
+      envFilePath: './apps/auth/.env',
+      isGlobal: true,
+    }),
+    // 2. Модуль для подключения к базе данных.
+    // Он использует загруженные переменные для конфигурации.
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT!, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true, // Автоматически находить и загружать все наши Entity.
+      synchronize: true, // Для разработки: автоматически создает/обновляет таблицы в БД.
+                         // ВАЖНО: на продакшене это нужно выключить и использовать миграции.
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AppModule {}
